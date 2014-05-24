214d5b29bb05ed1ed3154310ad9afdacc4dee890
kazlock <ebracho@mail.csuchico.edu>
2014-05-24 16:11:35 -0700

    Added basic irc connection functions: connect(), send_data(), login(), join_channel().

--- kazbot.py
+++ kazbot.py
@@ -1,8 +1,31 @@
 import socket
-from connection_settings import HOST, PORT, CHAN, nickname, username, realname
+from connection_settings import HOST, PORT, CHAN, nickname, username, realname, pwd, debug
+
+IRC = socket.socket()
+
+def connect():
+    IRC.connect((HOST, PORT))
+    if debug == True:
+        print ("Connected to: " + str(IRC.getpeername()))
+
+def send_data(msg):
+    IRC.send("%s\r\n" % msg)
+    if debug == True:
+        print("I> " + msg)
+
+def login():
+    if pwd != None: send_data("Pass %s" % pwd)
+    send_data("Nick %s" % nickname)
+    send_data("User %s 0 * :%s" % (username, realname))
+
+def join_channel():
+    send_data("Join %s" % CHAN)
 
 def main():
-    pass
+    connect()
+    login()
+    join_channel()
+
 
 if __name__ == "__main__":
     main()
